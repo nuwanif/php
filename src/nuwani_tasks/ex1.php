b29d43195b7aecd5dd45f941712519dbf3395e72
nuwani fernando <nuwanifernando86@outlook.com>
2024-01-31 12:26:39 +0200

    amended

--- src/nuwani_tasks/ex1.php
+++ src/nuwani_tasks/ex1.php
@@ -2,7 +2,7 @@
 $title="Exercise 1";
 include 'header.php';?>
 
-    <h3>3.1 Write PHP code to output the following message:
+    <h3 style="font style:15px;">3.1 Write PHP code to output the following message:
     Hello world! My name is "David"
     </h3>
     <?php
@@ -11,15 +11,15 @@ include 'header.php';?>
 
 ?>
 
-    <h3>Create a PHP variable named $title and assign it the value "PHP is interesting." 
+    <h3 style="font style:10px;">3.2 Create a PHP variable named $title and assign it the value "PHP is interesting." 
         Then, use this variable as the content within an(heading 4) element.</h3>
 
     <?php
     $title ="PHP is interesting.";
-    echo "<h4>$title</h4>";             
+    echo "<h4 font color:white>$title</h4>";             
 ?>
 
-<h3>3.3 Define three variables: $g1 = 5, $g2 = 4, and $g3 = 5. 
+<h3 style="font style:10px;">3.3 Define three variables: $g1 = 5, $g2 = 4, and $g3 = 5. 
     These variables represent the grades of three students in the course. To display this information, create an HTML table within your PHP code. The table should be structured with columns for 
     Serial Number (S.n.), Name, and Grade, and it should look like this:</h3>
 
@@ -28,7 +28,7 @@ include 'header.php';?>
     $g2=4;
     $g3=5;
     ?>
-    <table>
+    <table style="color: white";>
         <tr><th>S.n.</th><th>Name</th><th>Grade</th></tr>
         <tr>
             <td>1</td>
@@ -47,7 +47,7 @@ include 'header.php';?>
         </tr>
     </table>    
 
-  <h3>Take a screenshot that confirms your development environment setup and include it as an image in the "ex1.php" file.</h3> 
+  <h3 style="font style:10px;">Take a screenshot that confirms your development environment setup and include it as an image in the "ex1.php" file.</h3> 
   <img src="./images/24.png" alt="Web development">
   
   <?php include 'footer.php'; ?>
