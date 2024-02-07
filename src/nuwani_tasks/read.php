3126d7c4cc0637ca2ca336449f0b4bee899ad3a5
nuwani fernando <nuwanifernando86@outlook.com>
2024-02-08 01:38:38 +0200

    amend

--- src/nuwani_tasks/read.php
+++ src/nuwani_tasks/read.php
@@ -1,6 +1,6 @@
 <?php
 $title='read.php';
-include '../header.php';
+include 'header.php';
 include 'db.php';
 // SQL query to retrieve data from the 'studentsinfo' table
 $sql = "SELECT * FROM studentsinfo";
@@ -43,6 +43,6 @@ $conn->close();
 
 ?>
 <?php
-include '../footer.php';
+include 'footer.php';
 
 ?>
